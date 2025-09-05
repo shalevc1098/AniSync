8e7cad18de7ab4002b477647f1119254c3b2b991
shalevc1098 <shalevc8901@gmail.com>
2025-09-05 12:38:49 +0300

    Added session and distributed memory cache configuration

--- Shoko.MAL/PluginServiceRegistration.cs
+++ Shoko.MAL/PluginServiceRegistration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Shoko.AniSync.Controllers;
 using Shoko.Plugin.Abstractions;
+using System;
+using System.Reflection;
 
 namespace Shoko.AniSync
 {
@@ -12,9 +15,25 @@ namespace Shoko.AniSync
             services.AddHttpClient();
             services.AddHttpContextAccessor();
             services.AddMemoryCache();
+            
+            // Add session support for remembering authenticated users
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromDays(30); // Session persists for 30 days
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                options.Cookie.Name = ".Shoko.MAL.Session";
+            });
+            
+            // Note: Shoko's internal services like IUserDataService should be automatically available
+            // through the DI container if they're registered in Shoko Server
 
-            var mvc = services.AddControllers();
-            mvc.AddApplicationPart(typeof(AniSyncController).Assembly).AddControllersAsServices();
+            // Add MVC with Views support
+            var assembly = typeof(AniSyncController).Assembly;
+            var mvcBuilder = services.AddControllersWithViews();
+            
+            mvcBuilder.AddApplicationPart(assembly);
 
             services.AddHostedService<ShokoMalPlugin>();
         }
